183a82189affef3570d91f6481d57a4cab2afab0
Daiane Oliveira <dai.daianeaparecidaoliveira@gmail.com>
2018-08-03 16:14:03 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/cursomc/service/PedidoService.java
+++ src/main/java/com/cursomc/service/PedidoService.java
@@ -1,5 +1,7 @@
 package com.cursomc.service;
 
+import org.springframework.data.domain.Page;
+
 import com.cursomc.domain.Pedido;
 
 public interface PedidoService {
@@ -7,4 +9,6 @@ public interface PedidoService {
 	public Pedido find(Integer id);
 	
 	public Pedido insert(Pedido pedido);
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction);
 }
--- src/main/java/com/cursomc/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/cursomc/service/impl/PedidoServiceImpl.java
@@ -4,14 +4,19 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.ItemPedido;
 import com.cursomc.domain.PagamentoComBoleto;
 import com.cursomc.domain.Pedido;
 import com.cursomc.domain.enums.EstadoPagamento;
 import com.cursomc.repositories.PedidoRepository;
+import com.cursomc.security.UserSS;
 import com.cursomc.service.BoletoService;
 import com.cursomc.service.ClienteService;
 import com.cursomc.service.EmailService;
@@ -19,6 +24,7 @@ import com.cursomc.service.ItemPedidoService;
 import com.cursomc.service.PagamentoService;
 import com.cursomc.service.PedidoService;
 import com.cursomc.service.ProdutoService;
+import com.cursomc.service.exceptions.AuthorizationException;
 import com.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -83,6 +89,19 @@ public class PedidoServiceImpl implements PedidoService {
 		itemPedidoService.save(pedido.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(pedido);
 		return pedido;
+	}
+
+	@Override
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserServiceImpl.authenticated();
+		
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 
 }
